bf6342984d14650b7cc537df451245d96884626d
AmulyaMurahari <37552813+AmulyaMurahari@users.noreply.github.com>
2023-04-07 20:46:26 -0400

    Intial Commit for Landing Page

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/home.jsx'
+import Login from './Components/Login/login.jsx'
+import Sell from './Components/Sell/sell.jsx';
+import Cart from './Components/Cart/cart.jsx';
+import WishList from './Components/Wishlist/wishList.jsx';
+import Help from './Components/Help/help.jsx'
+import Categories from './Components/Categories/categories';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Footer from './Components/Footer/footer'
+function App() {
+  return (
+    <>
+      <Router>
+      <Navbar />
+      <div className="container">
+      <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sell" element={<Sell />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishList" element={<WishList />} />
+          <Route path="/help" element={<Help />}  />
+          <Route path="/categories" element={<Categories />}  />
+         </Routes>
+        <Footer />
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App;
