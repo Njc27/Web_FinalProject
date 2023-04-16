31ea7afb2039688eb770850d4fceb32e515f180a
SinchanaKumara <kumara.s@northeastern.edu>
2023-04-16 00:34:59 -0400

    changes to home

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -140,33 +140,39 @@ const home = () => {
         <Button variant="primary">Details</Button>
       </Card.Body>
     </Card><br></br></Col>
-        <Col><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={Table1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Details</Button>
-      </Card.Body>
-    </Card><br></br></Col>
-        <Col><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={Table1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Details</Button>
-      </Card.Body>
-    </Card><br></br></Col>
       </Row>
     </Container>
-    
     <br></br>
     
+    <div class="container-header">
+      <p class="section-head-p">Frequently asked questions</p>
+    </div>
+    <br>
+    </br>
+
+    {/* Accordian----- */}
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Q1. What is the cost of a NEU DEALS account ?</Accordion.Header>
+        <Accordion.Body>
+        It is free to register for an NEUDeals Business account. There’s no long-term commitment, and no minimum spend.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Q2. What kind of products are available to buy on NEU DEALS ?</Accordion.Header>
+        <Accordion.Body>
+        NEU DEALS is a website for all your day-to-day purchases, whether you’re buying home supplies, furnitures, or decor items. With access to tens of thousands of different suppliers, you’ll find everyday low business pricing on hundreds of millions of products. 
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Q3. Who buys on NEU DEALS ?</Accordion.Header>
+        <Accordion.Body>
+        NEU DEALS customers range from undergraduates to graduates within the NEU campus with tens of thousands of students. We serve millions of student customers around the campus with NEU login access.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    <br></br>
+
 
   </div>
   )
