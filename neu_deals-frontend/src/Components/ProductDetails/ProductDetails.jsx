733da2cf610e0c6ed2738b5fee60c68d838473af
Njc27 <jc.n@northeastern.edu>
2023-04-17 03:51:11 -0400

    front-end small details

---
+++ neu_deals-frontend/src/Components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import classes from'./ProductDetails.css'
+import Table1 from '../../Assests/Table1.jpg'
+import Table2 from '../../Assests/Table2.jpg'
+import ProductData from './ProductData'
+
+const ProductDetails = () => {
+  return (
+    <div className="MainContainer">
+        <div className="ProductPreview">
+            <img src={Table1} alt='Product Preview'/>
+        </div>
+       <div className="ProductData">
+        <h1 className="ProductTitle"> {ProductData.title} </h1>
+        <p className='ProductDescription'> {ProductData.description} </p>
+
+        <div>
+            <img className='ProductImage' src={Table2}></img>
+            <img className='ProductImage' src={Table2}></img>
+            <img className='ProductImage' src={Table2}></img>
+            <img className='ProductImage' src={Table2}></img>
+        </div>
+
+        <div>
+           <button className='AddCart'>Add To Cart</button>
+           <button className='AddCart'>Wishlist</button> 
+        </div>
+
+       </div>
+      </div>
+  )
+}
+
+export default ProductDetails
