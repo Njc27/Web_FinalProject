492dbc37268ff60bd9ccf7450a97dd3fe64c63f4
srushti-ghatage <122959667+srushti-ghatage@users.noreply.github.com>
2023-04-20 12:12:07 -0400

    updated category

--- neu_deals-frontend/src/Components/Sell/sell.jsx
+++ neu_deals-frontend/src/Components/Sell/sell.jsx
@@ -10,7 +10,7 @@ export default function(){
         },
         {
             categoryName:'Electronics',
-            categoryItems: ['Laptop','Mobile','Headphone']
+            categoryItems: ['Laptop','Mobile','Headphone','Protable Heaters']
         },
         {
             categoryName:'Books',
