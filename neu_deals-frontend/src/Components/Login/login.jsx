2c0f5dce3b7a2f547a6dee3e6e780c6e72c914a1
SinchanaKumara <kumara.s@northeastern.edu>
2023-04-19 23:34:59 -0400

    Login and signUp changes

--- neu_deals-frontend/src/Components/Login/login.jsx
+++ neu_deals-frontend/src/Components/Login/login.jsx
@@ -1,30 +1,75 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { useNavigate } from "react-router-dom";
 import "../Login/login.css"
+import { useDispatch,useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+import { loginUser } from '../../redux/actions/loginAction';
 
 const Login = () => {
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {userData} =  useSelector(state =>state.user);
+
+  useEffect(()=>{
+    console.log(sessionStorage.getItem("userId"))
+      if(sessionStorage.getItem("userId") !==undefined){
+        let obj = JSON.parse(sessionStorage.getItem("userId"));
+        console.log(obj)
+        if(obj?._id){
+          navigate('../home');
+        }
+      }
+  },[userData])
+
 
   const handleRegisterClick = () => {
     navigate('/SignUpUi');
   }
 
+
+const [isActive,setIsActive] = useState(false)
+  const handleSignIn = (e) =>{
+    e.preventDefault();
+    dispatch(loginUser(email,password));
+  }
+
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+
   return (
     <div className="center">
-      <h1>SIGN IN</h1>
+      <h1>Sign In</h1>
       <form method="post">
+      <div className='dropdown'>
+        <div className='dropdown-btn' onClick={(e) => setIsActive(!isActive)}>
+        What do you want to do today?<i class="fa fa-caret-down"></i>
+        </div>
+        {isActive &&
+          <div className='dropdown-content'>
+          <div className='dropdown-item'>
+            Buy
+          </div>
+          <div className='dropdown-item'>
+            Sell
+          </div>
+        </div>
+        }
+        
+      </div>
         <div className="txt_field">
-          <input type="text" required />
+          <input type="text" required  onChange={(e) =>{setEmail(e.target.value)}}/>
           <span></span>
-          <label>Username</label>
+          <label>Email</label>
         </div>
         <div className="txt_field">
-          <input type="password" required />
+          <input type="password" required onChange={(e) =>{setPassword(e.target.value)}}/>
           <span></span>
           <label>Password</label>
         </div>
         <div className="pass">Forgot Password?</div>
-        <input type="submit" value="Login" />
+        <input type="submit" value="Login" onClick={(e)=>handleSignIn(e)}/>
         <div className="signup_link">
           Not Registered yet? <a href="#" onClick={handleRegisterClick}>Register here</a>
         </div>
@@ -33,4 +78,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default Login
