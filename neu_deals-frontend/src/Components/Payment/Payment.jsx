22347fd15defcb2478f47eb3bc2d0cf3113415da
SinchanaKumara <kumara.s@northeastern.edu>
2023-04-20 15:46:59 -0400

    Final commit

--- neu_deals-frontend/src/Components/Payment/Payment.jsx
+++ neu_deals-frontend/src/Components/Payment/Payment.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Payment = () => {
+  
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    alert("Your payment was successful");
+    navigate('/home')
+  }
+  )
   return (
     <div>
-    Home
+
     </div>
   );
 };
